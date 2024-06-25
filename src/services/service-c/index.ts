109378e6a714d4b693540e59ecd4b7f6f73211da
Ilyas Ilyasov <ilyas.ilyasov.1@gmail.com>
2024-06-26 01:40:27 +0200

    feat: different route scenarios

--- src/services/service-c/index.ts
+++ src/services/service-c/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
-import { getExpressRouter } from '../../lib/express';
+import { getExpressRouter, routeWrapper } from '../../lib/express';
+import { sleep } from '../../lib/utils';
 
 export const getRoutes = () => {
     const router = getExpressRouter();
@@ -21,9 +22,14 @@ export const getRoutes = () => {
         //throw new Error('Error from Service C 1');
     });
 
-    router.get('/2', (req: Request, res: Response) => {
+    router.get('/routeWithError', (req: Request, res: Response) => {
         throw new Error('Error from Service C 2');
     });
+
+    router.get('/routeWithLongOperation', routeWrapper(async (data: any) => {
+        await sleep(1000);
+        return { success: true };
+    }));
 
     router.get('/1/1', (req: Request, res: Response) => {
         res.json({
