6867610d742f6a98001ed2ca3c54521de376024f
Ilyas Ilyasov <ilyas.ilyasov.1@gmail.com>
2024-06-26 05:37:24 +0200

    feat: without any tool, for load test

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,21 @@
 import { errorHandler, getExpressApp } from "./lib/express";
 import dotenv from 'dotenv';
-import { expressMiddleware } from 'zipkin-instrumentation-express';
-import { Zipkin } from "./lib/zipkin";
 
 dotenv.config();
 
 const start = async () => {
     const serviceName = process.env.SERVICE_TYPE;
     const port = process.env.PORT || 3000;
-    const zipkinBaseUrl = process.env.ZIPKIN_BASE_URL || 'http://localhost:9411';
 
     if (!serviceName) {
         throw new Error('SERVICE_TYPE is not defined');
     }
 
     console.log(`Serving ${serviceName}...`)
-
-    const tracer = Zipkin.getTracer(serviceName, zipkinBaseUrl);
     
 
     try {
         const app = getExpressApp();
-        
-        // Apply the Zipkin middleware
-        app.use(expressMiddleware({ tracer }));
 
         const service = await import(`./services/${serviceName}/index`);
         const router = await service.getRoutes();
